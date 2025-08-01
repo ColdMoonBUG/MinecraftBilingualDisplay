db17787cf5d67419ac1b2163c26f390f21f9c307
BUG <2991742315@qq.com>
2025-08-01 21:54:40 +0800

    First Commint

---
+++ src/main/java/top/lyhc/mcmod/TranslationTheItemClient.java
@@ -0,0 +1,19 @@
+package top.lyhc.mcmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @projectName: translationtheitem-template-1.20.1
+ * @package: top.lyhc.mcmod
+ * @className: TranslationTheItemClient
+ * @author: BUG
+ * @description: TODO
+ * @date: 2025/7/31 下午11:39
+ * @version: 1.0
+ */
+public class TranslationTheItemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
